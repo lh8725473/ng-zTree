49b994c777f133cf952f1e0e8a60df69bfa46fd1
lh8725473 <171213574@qq.com>
2014-11-24 10:59:44 +0800

    dome.html

---
+++ src/ng-zTree-directive.js
@@ -0,0 +1,13 @@
+angular.module('ngzTree', []).directive('zTree', ['$timeout',
+
+	function($timeout) {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+        $timeout(function(){
+          $.fn.zTree.init(element, scope[attrs.setting], scope[attrs.zNodes]);
+        })
+			}
+		}
+	}
+])
