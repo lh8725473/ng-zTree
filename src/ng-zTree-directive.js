ed8d12eefb422b889e85a2fe5c08ffa0cadc41f9
liuh <lh8725473@users.noreply.github.com>
2015-05-20 17:17:45 +0800

    Update ng-zTree-directive.js

--- src/ng-zTree-directive.js
+++ src/ng-zTree-directive.js
@@ -1,12 +1,11 @@
 angular.module('ngzTree', []).directive('zTree', ['$timeout',
-
 	function($timeout) {
 		return {
 			restrict: 'A',
 			link: function(scope, element, attrs) {
-        $timeout(function(){
-          $.fn.zTree.init(element, scope[attrs.setting], scope[attrs.zNodes]);
-        })
+				$timeout(function(){
+				  $.fn.zTree.init(element, scope[attrs.setting], scope[attrs.zNodes]);
+				})
 			}
 		}
 	}
